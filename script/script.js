21620e13cbd943566d34565bc15d3b851b1d4a2d
Unknown <ocebeki@gmail.com>
2018-07-03 20:02:29 +0100

    ready

---
+++ script/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+
+
+    $('.header__toggleButton').click(function () {
+        
+            event.stopPropagation();
+
+            $('.header__list').toggleClass('open');
+
+    
+    });
+    $('.header__item a').click(function () {
+        $('.header__list').removeClass('open');
+    });
+
+    $('body').click(function () {
+
+        if (
+            $('.header__list').hasClass('open') == true)
+            $('.header__list').removeClass('open');
+
+    });
+
+    $('.multi-item-carousel .item').each(function () {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        if (next.next().length > 0) {
+            next.next().children(':first-child').clone().appendTo($(this));
+        } else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+        }
+    });
+
+
+
+});

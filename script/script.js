6a9a72d5585182f27ec67d1755bbca5a9e07825d
Unknown <ocebeki@gmail.com>
2019-08-30 11:07:11 +0200

    corrections

--- script/script.js
+++ script/script.js
@@ -1,40 +1,40 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  $(".header__toggleButton").click(function() {
+    event.stopPropagation();
+    $(".header__list").toggleClass("open");
+  });
 
+  $(".header__item a").click(function() {
+    $(".header__list").removeClass("open");
+  });
 
-    $('.header__toggleButton').click(function () {
-        
-            event.stopPropagation();
+  $("body").click(function() {
+    if ($(".header__list").hasClass("open"))
+      $(".header__list").removeClass("open");
+  });
 
-            $('.header__list').toggleClass('open');
+  $(".multi-item-carousel .item").each(function() {
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+    next
+      .children(":first-child")
+      .clone()
+      .appendTo($(this));
 
-    
-    });
-    $('.header__item a').click(function () {
-        $('.header__list').removeClass('open');
-    });
-
-    $('body').click(function () {
-
-        if (
-            $('.header__list').hasClass('open') == true)
-            $('.header__list').removeClass('open');
-
-    });
-
-    $('.multi-item-carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-        } else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
-
-
-
+    if (next.next().length > 0) {
+      next
+        .next()
+        .children(":first-child")
+        .clone()
+        .appendTo($(this));
+    } else {
+      $(this)
+        .siblings(":first")
+        .children(":first-child")
+        .clone()
+        .appendTo($(this));
+    }
+  });
 });
